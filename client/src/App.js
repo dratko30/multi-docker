6899b8da5b28b5e86b887f18df0cc81f8b11a01f
Ratko Dimovski <d_ratko30@yahoo.com>
2022-06-20 13:05:53 +0200

    Popara

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Oba bese tesko po vtor pat!!!
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
